c861ada18978ec26d7babd70dd55512eb3d9d5d6
sampulsar <80544409+sampulsar@users.noreply.github.com>
2024-09-09 06:58:22 +0930

    StashDB Import

---
+++ src/NzbDrone.Core/ImportLists/StashDB/StashDBSettings.cs
@@ -0,0 +1,39 @@
+using FluentValidation;
+using NzbDrone.Core.Annotations;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Core.Validation;
+
+namespace NzbDrone.Core.ImportLists.StashDB
+{
+    public class StashDBSettingsValidator : AbstractValidator<StashDBSettings>
+    {
+        public StashDBSettingsValidator()
+        {
+            RuleFor(c => c.ApiKey)
+                .NotEmpty()
+                .WithMessage("Api Key must not be empty");
+        }
+    }
+
+    public class StashDBSettings : IProviderConfig
+    {
+        private static readonly StashDBSettingsValidator Validator = new StashDBSettingsValidator();
+
+        public StashDBSettings()
+        {
+            Filter = FavoriteFilter.ALL;
+            ApiKey = "";
+        }
+
+        [FieldDefinition(0, Label = "Api Key", Privacy = PrivacyLevel.ApiKey, HelpText = "Your StashDB Api Key")]
+        public string ApiKey { get; set; }
+
+        [FieldDefinition(1, Label = "Favorite Filter", Type = FieldType.Select, SelectOptions = typeof(FavoriteFilter), HelpText = "Filter by favorited entity")]
+        public FavoriteFilter Filter { get; set; }
+
+        public NzbDroneValidationResult Validate()
+        {
+            return new NzbDroneValidationResult(Validator.Validate(this));
+        }
+    }
+}
